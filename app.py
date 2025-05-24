c5ab857edc9c681b5d159b5cb6fb3d7a10f3a5f8
Johann Liwag <jojoliwag4@gmail.com>
2025-05-24 12:13:01 +0800

    lmao

--- app.py
+++ app.py
@@ -21,39 +21,41 @@ def userIsTheServer(addr: str) -> bool:
 
 # WEB ROUTES
 
-@app.route('/')
+# users will submit their name to route '/'
+@app.route('/', methods=['GET', 'POST'])
 def home():
     if userIsTheServer(request.remote_addr):
-        return f"{session_code}"
-    else:
-        return render_template('') # USER SESSION CODE PROMPT
+        return render_template('admin.html', session_code=session_code) # show the session code to the session master
+    
+    if request.method == 'POST':
+        name: str = request.form.get('name')
+        users["names"].append(name)
+        
+        with open('moment.json', 'w') as file:
+            json.dump(users, file, indent=2)
 
-@app.route('/login', methods=['POST'])
-def userLogin():
-    name: str = request.form.get('name')
-    users["names"].append(name)
+        return render_template('', name=name) # prompt for session code
+    
+    return render_template('index.html') # user prompt for name
 
-    with open('moment.json', 'w') as file:
-        json.dump(users, file, indent=2)
-
-    return redirect(url_for('begin_session'))
 
 @app.route('/submit_code', methods=['POST'])
 def submitSessionCode():
     user_input_code: str = request.form.get('session_code')
+    name = request.form.get('name')
+
     if user_input_code == session_code:
         return redirect(url_for()) # USER LOGIN PAGE
     else:
         return "WRONG SESSION CODE!!!"
 
-@app.route(f'/{session_code}')
+
+@app.route('/room')
 def begin_session():
     return render_template(
-        'index.html' if userIsTheServer(request.remote_addr) else 'admin.html', 
+        'admin_room.html' if userIsTheServer(request.remote_addr) else 'user_room.html',
         users=users
     )
-
-
 
 if __name__ == "__main__":
     app.run(
